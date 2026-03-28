849ff6f8fe37ba227de17189d85376058933673f
Robin-Hood-zjw <jiawenz@umich.edu>
2026-03-27 20:25:46 -0700

    0.2.3-implementing_Semaphore_methods

--- src/thread/semaphore.cpp
+++ src/thread/semaphore.cpp
@@ -2,6 +2,11 @@
 
 namespace sylar {
     /**
+     * @brief the constructor initializes the count variable with the given value
+     */
+    explicit Semaphore::Semaphore(int count=0) : _count(count) {}
+
+    /**
      * @brief the wait function will block the calling thread until the count is greater than zero. 
      * @details it uses a unique lock to protect the count variable and a condition variable to wait for signals. When the count is greater than zero, it will decrease the count and continue execution.
      */
@@ -9,17 +14,23 @@ namespace sylar {
         // lock the mutex to protect the count variable
         std::unique_lock<std::mutex> lock(_mutex);
 
-        // 
-        while (count == 0) { 
-            cv.wait(lock); // wait for signals
+        // wait until the count is greater than zero, if the count is zero, it will block the thread and wait for signals
+        while (_count == 0) { 
+            _cv.wait(lock); // wait for signals
         }
-        count--;
+        _count--;
     }
 
+    /**
+     * @brief the signal function will increase the count variable and wake up one waiting thread if there are any.
+     * @details it uses a unique lock to protect the count variable and a condition variable to notify one waiting thread.
+     */
     void Semaphore::signal() {
+        // lock the mutex to protect the count variable
         std::unique_lock<std::mutex> lock(_mutex);
 
-        count++;
-        cv.notify_one();
+        // increase the count and wake up one waiting thread if there are any
+        _count++;
+        _cv.notify_one();
     }
 }
--- src/thread/semaphore.h
+++ src/thread/semaphore.h
@@ -11,15 +11,15 @@ namespace sylar {
      */
     class Semaphore {
     private:
-        int count;                                                  // the number of available resources
+        int _count;                                                 // the number of available resources
 
         std::mutex _mutex;                                          // mutex to protect the count variable
-        std::condition_variable cv;                                 // condition variable to block and wake up threads
+        std::condition_variable _cv;                                // condition variable to block and wake up threads
     public:
-        explicit Semaphore(int count_ = 0) : count(count_) {}       // constructor to initialize the count
+        explicit Semaphore(int count=0);                            // constructor to initialize the count
 
-        void wait();                                                // wait for a signal, it will block if the count is zero, otherwise it will decrease the count and continue execution
-        void signal();                                              // signal a waiting thread, it will increase the count and wake up one waiting thread if there are any
+        void wait();                                                // wait for a signal to wake up
+        void signal();                                              // signal a waiting thread to wake up
     };
 }
 
